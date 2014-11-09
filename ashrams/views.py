609723b97ae1b304e8b2066ec7a4defe8fda27b1
challa913 <challa.rao3@gmail.com>
2014-11-09 17:26:55 +0530

    minor change in home html

--- ashrams/views.py
+++ ashrams/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render, render_to_response
 # Create your views here.
 
 def sample_html(request):
-    return render_to_response('dropdownheader.html')
+    return render_to_response('home.html')
